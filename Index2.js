e38b17426f0245097f8d4434aca044c1bd1d9fed
Tanishka <guptatanishka2008@gmail.com>
2025-01-25 23:29:50 +0530

    day-7

--- Index2.js
+++ Index2.js
@@ -52,3 +52,48 @@ return string === string.split('').reverse().join('');
 }
 
 console.log(checkPalindrome("appa"))
+
+//# 16 Implement a function that flattens a nested array in JavaScript, converting it into a single-level array. 
+//# method-1
+const flattenArray = (arr) => { 
+  return arr.flat(Infinity)
+}
+console.log(flattenArray([4,5,7,[2,8,4,7],[4,1,8,[5,8,1,7]]]));
+
+//# method-2
+const flat = function(arr, n){
+ if(n === 0){
+    return arr.slice()
+ }
+ let result = [];
+ for(let i = 0; i <arr.length; i++) {
+  if(Array.isArray(arr[i])){
+   const nested = flat(arr[i],  n - 1);
+   result.push(...nested)
+  }
+  else{
+    result.push(arr[i]);
+  }
+ }
+ return result;
+}
+console.log(flat([4,5,7,[2,8,4,7],[4,1,8,[5,8,1,7]]]))
+
+
+//# 17 Write a function that determines if two strings are anagrams of each other.
+//# method-1
+const checkAnagram = (str1, str2) => {
+ return str1.split('').sort().join('') === str2.split('').sort().join('');
+}
+console.log(checkAnagram("abbi", "bbaa"));
+
+// //# method-2
+const checkAnagram1 = (str1, str2) => {
+let result = {};
+if(str1.length !== str2.length) return false;
+
+for (char of str1) {
+    
+}
+}
+console.log(checkAnagram1("abbi", "bbaa"));
